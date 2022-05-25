9b185230f80e6ae832b2f1286f5771a488e7331b
Ikem <cliroaganachi@gmail.com>
2022-05-25 15:07:14 +0100

    Response from server fixed

--- Back-End/Routes/routes.js
+++ Back-End/Routes/routes.js
@@ -1,75 +1,96 @@
-const {Router} = require('express');
+const {
+    Router
+} = require('express');
 
 const router = Router();
 const bcrypt = require('bcrypt');
 const signUpTemplateCopy = require('../Models/models');
 const UserData = require('../Models/models');
-const { default: mongoose } = require('mongoose');
+const {
+    default: mongoose
+} = require('mongoose');
 
 const saltRounds = 10;
 
 router.route('/')
-.get((req,res)=>{
-    res.send("Hello World");
-})
+    .get((req, res) => {
+        res.send("Hello World");
+    })
 
 router.route("/Register")
-.get((req,res)=>{
+    .get((req, res) => {
 
-})
-.post( async (req,res,next)=>{
-    try{
-         bcrypt.hash(req.body.password, saltRounds, (err,hash)=>{
-            const user = new signUpTemplateCopy({
-                firstName : req.body.firstName,
-                lastName : req.body.lastName,
-                userName : req.body.username,
-                email : req.body.email,
-                password : hash
-            });
-            console.log(user);
-        const createdUser = user.save();
-        
-        res.json(createdUser);
-        
-        })
-        
-    }catch(error){
-        if(error.constructor.name === 'ValidationError'){
-            res.status(422);
+    })
+    .post(async (req, res, next) => {
+        try {
+            bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+                const user = new signUpTemplateCopy({
+                    firstName: req.body.firstname,
+                    lastName: req.body.lastname,
+                    userName: req.body.username,
+                    email: req.body.email,
+                    password: hash
+                });
+
+                user.save();
+                console.log(user);
+
+                const createdUser = user;
+                res.json({
+                    username: createdUser.userName,
+                    firstName: createdUser.firstName,
+                    lastName: createdUser.lastName
+                })
+
+            })
+
+        } catch (error) {
+            if (error.constructor.name === 'ValidationError') {
+                res.status(422);
+            }
+            next(error);
+
         }
-        next(error);
-        
-    }
-    
-})
+
+    })
 
 
 router.route('/Login')
-.post((req,res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
+    .post(async (req, res, next) => {
+        try {
+            const username = req.body.username;
+            const password = req.body.password;
 
-    UserData.findOne({userName : username},(err,foundUser)=>{
-        if (err){
-            console.log(err);
-        }
-        else if(!foundUser){
-            console.log("Username mismatch");
-        }
-        else{
-            if (foundUser){
-                bcrypt.compare(password, foundUser.password, (err, result)=>{
-                    if(result ===true){
-                        console.log("Logged in successfully");
-                    }else{
-                        console.log("Wrong username or password");
+            UserData.findOne({
+                userName: username
+            }, (err, foundUser) => {
+                if (err) {
+                    console.log(err);
+                } else if (!foundUser) {
+                    res.json({message : "User does not exist"});
+                } else {
+                    if (foundUser) {
+                        bcrypt.compare(password, foundUser.password, (err, result) => {
+                            if (result === true) {
+                                res.json({message : "Username and password match"})
+                                console.log("Logged in successfully");
+                                
+                            } else {
+                                res.json({message : "Wrong username or password"})
+                                console.log("Wrong username or password");
+                            }
+                        })
                     }
-                } )
+                }
+
+            })
+        } catch(error) {
+            if(error.constructor.name === 'ValidationError'){
+                res.status(422);
             }
+            next(error);
+
         }
-        
     })
-})
 
 module.exports = router;
